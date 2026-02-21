9b24907b2e0876604df7626b27019ebd0b9bd884
Sander Constantin <sander.ctin@gmail.com>
2026-02-21 07:48:50 +0100

    Add `IdentityController` with Login and Register endpoints

---
+++ backend/Runnables.API/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using BL.Identity.Handlers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Runnables.API.Controllers;
+
+[Controller, AllowAnonymous, Route("/[controller]/[action]")]
+public class IdentityController : ControllerBase
+{
+    [HttpPost, ProducesDefaultResponseType(typeof(LoginRequestHandler.LoginResponse))]
+    public async Task<IActionResult> Login([FromServices] LoginRequestHandler handler,
+        [FromBody] LoginRequestHandler.LoginRequest request)
+        => Ok(await handler.Handle(request));
+
+    [HttpPost, ProducesDefaultResponseType(typeof(IdentityResult))]
+    public async Task<IActionResult> Register([FromServices] RegisterRequestHandler handler,
+        [FromBody] RegisterRequestHandler.RegisterRequest request)
+        => Ok(await handler.Handle(request));
+}
